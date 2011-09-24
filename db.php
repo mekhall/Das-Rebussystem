f65759606f3fb9b7bdfb0940d5071c1e5f1bbac1
Fredrik Hallenberg <hallon@debian.org>
2011-09-24 13:28:53 +0200

    Updated db and remove old vt2009 as it is not updated.

--- db.php
+++ db.php
@@ -4,64 +4,83 @@ require_once "rebus.php";
 
 define('SQLITE_DB', DATAROOT . '/' . SHORT_NAME . '/db');
 
-function setTestData()
+function create()
 {
-    for ($event = 0; $event <= count($GLOBALS['events']) - 1; ++$event) {
-	$d = file(DATA_PATH . $event . '.txt');
-	$data = preg_split("/\s/", $d[0]);
-	for ($team = 0; $team <= count($GLOBALS['teams']) - 1; ++$team) {
-	    setPoints($team, $event, $data[$team]);
-	}
-	echo "Event $event<br>";
+    $db = new SQLiteDatabase(SQLITE_DB);
+
+    $columns = array();
+    $values = array();
+    for ($event = 0; $event < count($GLOBALS['events']); ++$event) { 
+        $columns[] = "event$event INTEGER";
+	$values[] = "NULL";
+    }
+
+    $c = "CREATE TABLE rebus (team INTEGER PRIMARY KEY, " . implode(',', $columns) . ")";
+    $db->queryExec($c);
+
+    for ($team = 0; $team < count($GLOBALS['teams']); ++$team) {
+	$db->queryExec("INSERT INTO rebus VALUES($team, " . implode(',', $values) . ")");
     }
 }
 
-function checkAndCreate($team, $event)
+function getDb()
 {
-    $db = new SQLiteDatabase(SQLITE_DB);
-    $row = $db->singleQuery("SELECT * FROM sqlite_master WHERE name='team$team'");
-    if (count($row) == 0) {
-	$db->queryExec("CREATE TABLE team$team (event int, points int)");
-	for ($i = 0; $i <= $event; ++$i) {
-	    $db->queryExec("INSERT INTO team$team VALUES ($i, NULL)");
+    static $instance;
+    if (!isset($instance)) {
+	if (!is_readable(SQLITE_DB)) {
+	    create();
+	}
+	$instance = new SQLiteDatabase(SQLITE_DB);
+    }
+    return $instance;
+}
+
+function convert()
+{
+    copy(SQLITE_DB, SQLITE_DB . '.old');
+
+    $db = getDb();
+
+    create();
+
+    for ($team = 0; $team < count($GLOBALS['teams']); ++$team) {
+	for ($event = 0; $event < count($GLOBALS['events']); ++$event) { 
+	    $p = $db->singleQuery("SELECT points FROM team$team WHERE event=$event");
+	    setPoints($team, $event, $p);
 	}
     }
-    else {
-	$result = $db->query("SELECT * FROM team$team");
-	$n = $result->numRows();
-	if ($n <= $event) {
-	    for ($i = $n; $i <= $event; ++$i) {
-		$db->queryExec("INSERT INTO team$team VALUES ($i, NULL)");
-	    }
-	}
+    for ($team = 0; $team < count($GLOBALS['teams']); ++$team) {
+	$db->queryExec("DROP TABLE team$team");
     }
 }
 
 function setPoints($team, $event, $points)
 {
-    checkAndCreate($team, $event);
+    $db = getDb();
 
     if ($points == '') {
 	$points = 'NULL';
     }
 
-    $db = new SQLiteDatabase(SQLITE_DB);
-    $db->queryExec("UPDATE team$team SET points=$points WHERE event=$event");
+    if ($points != 'NULL' and !is_numeric($points)) {
+	return;
+    }
+
+     $db->queryExec("UPDATE rebus SET event$event=$points WHERE team=$team");
 }
 
 function getPoints($team, $event)
 {
-    checkAndCreate($team, $event);
+    $db = getDb();
 
-    $dbh = new SQLiteDatabase(SQLITE_DB);
-    $row = $dbh->singleQuery("SELECT points FROM team$team WHERE event=$event");
+    $row = $db->singleQuery("SELECT event$event FROM rebus WHERE team=$team");
 
     return $row;
 }
 
 function getEventPoints($event, $event2 = null)
 {
-    for ($team = 0; $team <= count($GLOBALS['teams']) - 1; ++$team) {
+    for ($team = 0; $team < count($GLOBALS['teams']); ++$team) {
 	if (is_null($event2)) {
 	    $result[$team] = getPoints($team, $event);
 	}
@@ -76,7 +95,7 @@ function getEventPoints($event, $event2 = null)
 function getAvgEventPoints($event, $event2 = null)
 {
     $result = 0;
-    for ($team = 0; $team <= count($GLOBALS['teams']) - 1; ++$team) {
+    for ($team = 0; $team < count($GLOBALS['teams']); ++$team) {
 	if (is_null($event2)) {
 	    $result += getPoints($team, $event);
 	}
@@ -89,7 +108,7 @@ function getAvgEventPoints($event, $event2 = null)
 
 function updateEventPoints(&$data, $event)
 {
-    for ($team = 0; $team <= count($GLOBALS['teams']) - 1; ++$team) {
+    for ($team = 0; $team < count($GLOBALS['teams']); ++$team) {
 	if (!isset($data[$team])) {
 	    $data[$team] = 0;
 	}
